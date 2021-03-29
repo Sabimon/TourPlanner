d3ca9e5a2fe0afba159b6583fed1d4974174fcb5
Sabrina Duchkowitsch <sabrina_duchkowitsch@hotmail.com>
2021-03-29 17:13:41 +0200

    database connection and save route names

--- TourPlannerDL/DBConn.cs
+++ TourPlannerDL/DBConn.cs
@@ -1,8 +1,38 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 
 namespace TourPlannerDL
 {
     public class DBConn
     {
+        string connString = "Host=localhost;Username=postgres;Password=wordpass;Database=TourPlanner;Port=5432";
+        private NpgsqlConnection conn;
+
+        public void Connection()
+        {
+            try
+            {
+                conn = new NpgsqlConnection(connString);
+                conn.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+        public DBConn()
+        {
+            Connection();
+        }
+
+        public void InsertNewRoute(string fileName)
+        {
+            using (var cmd = new NpgsqlCommand($"INSERT INTO routes (routename) VALUES (@r)", conn))
+            {
+                cmd.Parameters.AddWithValue("r", fileName);
+                cmd.ExecuteNonQuery();
+            }
+        }
     }
 }
