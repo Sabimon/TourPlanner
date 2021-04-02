7ee21e945566d1b2a8263e08207019fa9051c3d7
Sabrina Duchkowitsch <sabrina_duchkowitsch@hotmail.com>
2021-04-02 14:27:21 +0200

    class seperations for config

--- TourPlannerDL/DBConn.cs
+++ TourPlannerDL/DBConn.cs
@@ -9,51 +9,15 @@ using System.Xml.Serialization;
 
 namespace TourPlannerDL
 {
-    public class connectionStringsConfig
-    {
-        public string Host { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Database { get; set; }
-        public string Port { get; set; }
-    }
-
-    public class CustomConfigLoader : IConfigurationSectionHandler
-    {
-        public object Create(object parent, object configContext, XmlNode section)
-        {
-            if (section == null)
-            {
-                throw new ArgumentNullException($"XMLNode passed in is null.");
-            }
-
-            var type = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).FirstOrDefault(t => t.Name == section.Name);
-
-            if (type == null)
-            {
-                throw new ArgumentException($"Type with name {section.Name} couldn't be found.");
-            }
-
-            XmlSerializer ser = new XmlSerializer(type, new XmlRootAttribute(section.Name));
-
-            using (XmlReader reader = new XmlNodeReader(section))
-            {
-                return ser.Deserialize(reader);
-            }
-        }
-    }
-
     public class DBConn
     {
         private NpgsqlConnection conn;
-        //public string config = @"C:\Users\Lenovo\source\repos\TourPlanner\TourPlannerDL\App.config";
-        //public string connString = "Host=localhost;Username=postgres;Password=wordpass;Database=TourPlanner;Port=5432";
 
         public void Connection()
         {
             try
             {
-                var config = (connectionStringsConfig)ConfigurationManager.GetSection(nameof(connectionStringsConfig));
+                var config = (ConnectionStringsConfig)ConfigurationManager.GetSection(nameof(ConnectionStringsConfig));
                 string connString = $"Host={config.Host};Username={config.Username};Password={config.Password};Database={config.Database};Port={config.Port}";
                 conn = new NpgsqlConnection(connString);
                 conn.Open();
