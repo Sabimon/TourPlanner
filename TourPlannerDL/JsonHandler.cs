10755fadc0be40b61cd845f82abbb335c1ee990b
Sabrina Duchkowitsch <sabrina_duchkowitsch@hotmail.com>
2021-05-02 18:07:20 +0200

    Add/change/delete log controls added

--- TourPlannerDL/JsonHandler.cs
+++ TourPlannerDL/JsonHandler.cs
@@ -1,5 +1,4 @@
 ﻿using Newtonsoft.Json.Linq;
-using JsonSerializer = Newtonsoft.Json.JsonSerializer;
 
 namespace TourPlannerDL
 {
@@ -9,7 +8,6 @@ namespace TourPlannerDL
         public void DeserializeJSON(string json)
         {
             var jsonData = JObject.Parse(json);
-            JsonSerializer serializer = new JsonSerializer();
             var distance = jsonData["route"]["distance"].ToString();
             var totalTime = jsonData["route"]["formattedTime"].ToString();
             var highway = jsonData["route"]["hasHighway"].ToString();
